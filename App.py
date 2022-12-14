ec8a78a2c6c6d00b7cd8afd0ae5ff43cb0468c18
deepurala <pradeep.urala@outlook.com>
2022-12-14 21:58:47 +0000

    Updated app.py file to add insert function and modified index.html file to replicate this.

--- App.py
+++ App.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_mysqldb import MySQL
 
 app = Flask(__name__)
+app.secret_key = "flash message"
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = ''
@@ -9,9 +10,28 @@ app.config['MYSQL_DB'] = 'crudapplication'
 
 mysql = MySQL(app)
 
+@app.route('/insert', methods = ['POST'])
+def insert():
+
+    if request.method == "POST":
+        flash("Data Inserted Succesfully")
+
+        productname = request.form['productname']
+        productdescription = request.form['productdescription']
+        productprice = request.form['productprice']
+
+        cur = mysql.connection.cursor()
+        cur.execute("INSERT INTO product (product_name, product_description, product_price) VALUES (%s, %s, %s)", (productname, productdescription, productprice))
+        mysql.connection.commit()
+        return redirect(url_for('Index'))
+
 @app.route('/')
 def Index():
-    return render_template('index.html')
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT * FROM product")
+    data = cur.fetchall()
+    cur.close()
+    return render_template('index.html', product = data)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0',port=8080, debug=True)
