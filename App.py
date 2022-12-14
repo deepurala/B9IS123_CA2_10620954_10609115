0de38a5024eba1151f35b461e45d7aad30b58463
Devikabt <btdevika@gmail.com>
2022-12-14 21:01:18 +0000

    Updated database connection in app.py file

--- App.py
+++ App.py
@@ -1,10 +1,17 @@
 from flask import Flask, render_template
+from flask_mysqldb import MySQL
 
 app = Flask(__name__)
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'crudapplication'
+
+mysql = MySQL(app)
 
 @app.route('/')
 def Index():
     return render_template('index.html')
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0',port=8080, debug=True)
